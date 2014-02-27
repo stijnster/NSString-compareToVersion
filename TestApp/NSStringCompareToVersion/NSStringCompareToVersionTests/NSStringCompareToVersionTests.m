1b6b21ab8b1cda5a5054ed974a3be6b0eeb79ed1
Stijn Mathysen <stijn@skylight.be>
2014-02-27 22:27:25 +0100

    added working tests

--- TestApp/NSStringCompareToVersion/NSStringCompareToVersionTests/NSStringCompareToVersionTests.m
+++ TestApp/NSStringCompareToVersion/NSStringCompareToVersionTests/NSStringCompareToVersionTests.m
@@ -1,34 +1,51 @@
 //
-//  NSStringCompareToVersionTests.m
-//  NSStringCompareToVersionTests
+//  NSString+CompateToVersionTest.m
+//  NSStringCompareToVersion
 //
 //  Created by Stijn Mathysen on 27/02/14.
 //  Copyright (c) 2014 Skylight. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
+#import "NSString+CompareToVersion.h"
 
-@interface NSStringCompareToVersionTests : XCTestCase
+@interface NSString_CompateToVersionTest : XCTestCase
 
 @end
 
-@implementation NSStringCompareToVersionTests
+@implementation NSString_CompateToVersionTest
 
-- (void)setUp
-{
+-(void)setUp{
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+- (void)tearDown{
     [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+-(void)testCompareToVersionAscending{
+    [self compareVersion:@"1" withVersion:@"1.1" expectedResult:NSOrderedAscending];
+    [self compareVersion:@"1.1" withVersion:@"1.2" expectedResult:NSOrderedAscending];
+    [self compareVersion:@"6.4" withVersion:@"6.4.1" expectedResult:NSOrderedAscending];
+}
+
+-(void)testCompareToVersionDescending{
+    [self compareVersion:@"1.12" withVersion:@"1.1" expectedResult:NSOrderedDescending];
+    [self compareVersion:@"6.2" withVersion:@"5.4.3.2" expectedResult:NSOrderedDescending];
+    [self compareVersion:@"5.4.3.2" withVersion:@"5.0" expectedResult:NSOrderedDescending];
+}
+
+-(void)testCompareToVersionSame{
+    [self compareVersion:@"1.2.3.123" withVersion:@"1.2.3.123" expectedResult:NSOrderedSame];
+    [self compareVersion:@" 1.2.3.123 " withVersion:@"1.2.3.123" expectedResult:NSOrderedSame];
+}
+
+-(void)compareVersion:(NSString *)version withVersion:(NSString *)compareVersion expectedResult:(NSComparisonResult)expectedResult{
+    NSComparisonResult actualResult = [version compareToVersion:compareVersion];
+    
+    if(actualResult != expectedResult){
+        XCTFail(@"Compared %@ to %@, expected %d but got %d", version, compareVersion, expectedResult, actualResult);
+    }
 }
 
 @end
